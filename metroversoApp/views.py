44e08855ea6286ef493c86b3b7a298da51e8fd5e
lacastrilf <lacastrilf@eafit.edu.co>
2025-08-02 18:52:12 -0500

    Data mapping

--- metroversoApp/views.py
+++ metroversoApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .assets import stationGraphs
+
 def map(request):
     # Render the map template
     return render(request, 'map.html')
